22f756641212264ba1136dda613cd456f5fabf1a
Cesar Cruz <cesarcruzrodriguez@gmail.com>
2016-03-19 15:41:19 +0000

    corrección errores. Las Tablas funcionan perfectamente.

--- app/src/main/java/cruz/cesar/com/kangoogift/db/DB_Helper.java
+++ app/src/main/java/cruz/cesar/com/kangoogift/db/DB_Helper.java
@@ -12,7 +12,6 @@ public class DB_Helper extends SQLiteOpenHelper{
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "KabgooGift.lite";
 
-
     public DB_Helper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
